43421bcaec168d73cef3aa504247379ff5ef566d
chaudhry123 <fachaudhry96@gmail.com>
2024-04-29 15:33:56 -0400

    Update Alignment.bash

--- Single-cell-RNA-seq-Processing/Alignment.bash
+++ Single-cell-RNA-seq-Processing/Alignment.bash
@@ -1,4 +1,4 @@
-#Select fastq files and use script similar to this
+#Select fastq files and use script similar to this. Fastq files are located in the box
 cellranger count --id=Sample \
 --refdata-gex-GRCh38-2020-A/ \
 --fastqs=Fastq Files
